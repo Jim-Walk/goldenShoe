1c5a43a420c0e21edee8e70feb1284f8acd1c268
andrewpsuedonym <j.m.walker@live.co.uk>
2019-07-03 17:30:20 +0100

    update appspec, begin adding content to website

--- routes.py
+++ routes.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+from pymongo import MongoClient
+client = MongoClient()
+
+shoes_c = client.GoldenShoe.shoes
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    all_shoes = []
+    for shoe in shoes_c.find():
+        all_shoes += shoe
+    return render_template('index.html', all_shoes=all_shoes)
+
+@app.route('/shoe/<int:SHOE_ID>')
+def shoe(SHOE_ID):
+    shoe = shoes_c.find_one({'ID':SHOE_ID})
+    return render_template('shoe.html', shoe=shoe)
