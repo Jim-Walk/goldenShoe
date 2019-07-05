1070510a2a62a504d72754842b5a13c4d8aabf72
andrewpsuedonym <j.m.walker@live.co.uk>
2019-07-05 14:53:57 +0100

    demo

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template
-app = Flask(__name__)
 from pymongo import MongoClient
+from util.db_utils import decrease_stock
+app = Flask(__name__)
 client = MongoClient()
 
 shoes_c = client.GoldenShoe.shoes
@@ -18,6 +19,11 @@ def index():
 def shoe(SHOE_ID):
     shoe = shoes_c.find_one({'ID':SHOE_ID})
     return render_template('shoe.html', shoe=shoe)
+
+@app.route('/buy/<int:SHOE_ID>')
+def buy(SHOE_ID):
+    decrease_stock(SHOE_ID)
+    return render_template('buy.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
